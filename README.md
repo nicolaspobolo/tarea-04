cfd7cc946823e00d79ccf82302109f0c1e9bf224
pobolonicolas <pobolonicolas@gmail.com>
2022-09-24 11:36:48 -0300

    Creacion de clases

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # tarea-04
+
+Hoy en la clase usamos script defer, como referencia el siguiente cuadro
+
+<img src="./docs/img/script_tag.png"/>
+
+## Ejercicio
+
+* Crear objetos usando clases de los siguientes componentes:
+
+* Video
+* Audio
+* Boton
+* Lista de elemetos
---
+++ js/app.js
@@ -0,0 +1,33 @@
+class boton{
+    constructor(texto, color, alto, ancho){
+        this.texto=texto;
+        this.color=color;
+        this.alto=alto;
+        this.ancho=ancho;
+    }
+}
+
+class video{
+    constructor(fuente, resolucion, formato){
+        this.fuente=fuente;
+        this.resolucion=resolucion;
+        this.formato;
+    }
+}
+
+class audio{
+    constructor(fuente, formato){
+        this.fuente=fuente;
+        this.formato=formato;
+    }
+}
+
+class listaElementos{
+    constructor(tipoElemento, cantElementos){
+        this.tipoElemento=tipoElemento;
+        this.cantElementos=cantElementos;
+    }
+}
+
+
+
